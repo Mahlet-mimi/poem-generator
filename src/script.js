15c2760e0609e312b2328736c823f0eb605583b2
Mahlet-mimi <mimisha1513@gmail.com>
2025-09-05 08:48:14 -0700

    Add AI API integration

--- src/script.js
+++ src/script.js
@@ -1,13 +1,30 @@
-function poemGenerate(event) {
-  event.preventDefault();
+function displayPoem(response) {
+
   new Typewriter("#poem", {
-    strings: ["Piling dirt on dirt"],
+    strings: response.data.answer,
     autoStart: true,
     delay: 5,
     cursor: null,
   });
-  
 }
+
+
+function poemGenerate(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#user-instruction");
+  apiKey = "cc127dfco815025900e3bb779a4actf3"; 
+  let prompt = `User instruction: Generate Ethiopian poem about ${instructionInput.value}`;
+  let context =
+    "Your mission is to generate very short ethiopian poem only 4 lines and separate each lines in <br \> and sign in with 'SheCodes AI' with <strong> element not in the beginning but the bottom of that poem";
+  apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
+
+  console.log(context);
+  console.log(prompt);
+}
+
 
 let poemFormElement = document.querySelector("#poem-form");
 poemFormElement.addEventListener("submit", poemGenerate);
