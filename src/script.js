3aa98931829cdf22cc9b86db40fcc19b7475dccb
Mahlet-mimi <mimisha1513@gmail.com>
2025-09-06 02:39:26 -0700

    Add user experience

--- src/script.js
+++ src/script.js
@@ -1,5 +1,4 @@
 function displayPoem(response) {
-
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
@@ -13,16 +12,17 @@ function poemGenerate(event) {
   event.preventDefault();
 
   let instructionInput = document.querySelector("#user-instruction");
-  apiKey = "cc127dfco815025900e3bb779a4actf3"; 
+  apiKey = "cc127dfco815025900e3bb779a4actf3";
   let prompt = `User instruction: Generate Ethiopian poem about ${instructionInput.value}`;
   let context =
     "Your mission is to generate very short ethiopian poem only 4 lines and separate each lines in <br \> and sign in with 'SheCodes AI' with <strong> element not in the beginning but the bottom of that poem";
   apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayPoem);
-
-  console.log(context);
-  console.log(prompt);
+   axios.get(apiUrl).then(displayPoem);
+ 
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating an Ethiopian poem about ${instructionInput.value}</div>`;
 }
 
 
